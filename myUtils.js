0caf3acf81df27551d1ff0f79ccfb26a72816893
devadula-nandan <47176249+devadula-nandan@users.noreply.github.com>
2021-07-06 11:58:59 +0530

    Update myUtils.js

--- myUtils.js
+++ myUtils.js
@@ -2,17 +2,22 @@
 const qs = (query) => document.querySelector(`${query}`);
 const qsa = (query) => document.querySelectorAll(`${query}`);
 const ce = (element) => document.createElement(`${element}`);
-const lsgi = (key) => {//returns or gets array of corresponding key from ls
+
+//returns or gets array of corresponding key from ls
+const lsgi = (key) => {
     if (localStorage.getItem(key) === null) {
         return key = [];
     } else {
         return key = JSON.parse(localStorage.getItem(key));
     }
 }
-const lssi = (key, value) => {//sets the modified array(value) to the corresponding key in ls
+
+//sets the modified array(value) to the corresponding key in ls
+const lssi = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
 }
-//an rng function
+
+//an rng function , give parameters of lower and upper and it generates a random number within/including limits
 const rng = (lower, upper) => Math.floor(lower + (upper + 1 - lower) * Math.random());
 
 
